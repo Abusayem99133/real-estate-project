39403fd66370cd862dc3cd2c42e4a7e79f7ad2a2
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-04-09 11:38:09 +0600

    banner section slider work on going. nb:70% done

--- src/HomePage/Banner.jsx
+++ src/HomePage/Banner.jsx
@@ -1,13 +1,48 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/bundle";
 const Banner = () => {
   return (
-    <div>
-      <h2 className="text-4xl">ami ashatase navbar</h2>
-      <h2 className="text-4xl">ami ashatase navbar</h2>
-      <h2 className="text-4xl">ami ashatase navbar</h2>
-      <h2 className="text-4xl">ami ashatase navbar</h2>
-      <h2 className="text-4xl">ami ashatase navbar</h2>
-      <h2 className="text-4xl">ami ashatase navbar</h2>
-      <h2 className="text-4xl">ami ashatase navbar</h2>
+    <div className=" ml-[340px]">
+      <Swiper navigation={true} modules={[Navigation]} loop={true}>
+        <SwiperSlide>
+          <div
+            className="   min-h-screen"
+            style={{
+              backgroundImage:
+                "url(https://i.postimg.cc/m2sKmRr5/pexels-binyamin-mellish-186077.jpg) ",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <h1>Slide 1</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className=" min-h-screen  "
+            style={{
+              backgroundImage:
+                "url(https://i.postimg.cc/R07HQcp8/pexels-binyamin-mellish-1396132.jpg)",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <h1>Slide 2</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="min-h-screen"
+            style={{
+              backgroundImage:
+                "url(https://i.postimg.cc/sx4zT2C3/pexels-pixabay-164558.jpg)",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <h1>Slide 3</h1>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
