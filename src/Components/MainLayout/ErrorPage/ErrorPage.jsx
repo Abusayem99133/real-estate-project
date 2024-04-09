a7120cc9f5bf335dfc51a23e99f3dfa330f55a56
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-04-09 09:29:41 +0600

    errorpage work done

--- src/Components/MainLayout/ErrorPage/ErrorPage.jsx
+++ src/Components/MainLayout/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,14 @@
+import { TbFaceIdError } from "react-icons/tb";
 const ErrorPage = () => {
   return (
-    <div>
-      <h1 className="text-4xl">404</h1>
-      <h3 className="text-3xl">Not Found</h3>
+    <div className="text-center font-extrabold text-[140px]">
+      <h1>404.....!</h1>
+      <div className="flex items-center ml-64">
+        <h3 className="text-[80px]">Not Found....</h3>
+        <p>
+          <TbFaceIdError></TbFaceIdError>
+        </p>
+      </div>
     </div>
   );
 };
--- src/NavbarSection/Navbar.jsx
+++ src/NavbarSection/Navbar.jsx
@@ -1,6 +1,24 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/house.png";
 const Navbar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About Us</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">Contact Us</NavLink>
+      </li>
+      <li>
+        <NavLink to="/profile">Update Profile</NavLink>
+      </li>
+    </>
+  );
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-200 ">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -23,52 +41,18 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navLinks}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className=" text-xl">
+          <img className="w-16" src={logo} alt="" />
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1 gap-4">{navLinks}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn bg-orange-200">Login</a>
       </div>
     </div>
   );
